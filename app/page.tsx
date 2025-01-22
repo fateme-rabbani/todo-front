0315ceb508a7d7e1050433fa3839fa4b5eaaf465
fatemeh <fatemeh.rabbani.75@gmail.com>
2025-01-22 20:09:36 +0330

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,103 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+type Status = "todo" | "doing" | "done";
+interface Todo {
+  id: number;
+  name: string;
+  i_status: Status;
+}
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todoText, setTodoText] = useState<string>("");
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  useEffect(() => {
+    const fetchTodos = async () => {
+      const result = await axios.get("http://localhost:1337/api/todos");
+      setTodos(result.data.data);
+    };
+    fetchTodos();
+  }, []);
+
+  const addTodo = async () => {
+    if (todoText.trim().length > 0) {
+      const result = await axios.post("http://localhost:1337/api/todos", {
+        data: {
+          name: todoText,
+          i_status: "todo",
+        },
+      });
+
+      setTodos([...todos, result.data.data]);
+      setTodoText(""); // Reset input after adding
+    }
+  };
+
+  const deleteTodoItem = async (id: number) => {
+    if (confirm("Do you really want to delete this item?")) {
+      const a = await axios.delete(`http://localhost:1337/api/todos/${id}`);
+      console.log(a);
+      setTodos(todos.filter((_todo) => _todo.id !== id));
+    }
+  };
+
+  const changeStatus = async (id: number, status: Status) => {
+    const result = await axios.put(`http://localhost:1337/api/todos/${id}`, {
+      i_status: status,
+    });
+    console.log(result);
+
+    setTodos(
+      todos.map((_todo) => (_todo.id === id ? result?.data?.data : _todo))
+    );
+  };
+
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          addTodo();
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Add new todo here..."
+          value={todoText}
+          onChange={(e) => setTodoText(e.target.value)}
+        />
+        <button type="submit">Add Todo</button>
+      </form>
+
+      <ul>
+        {["todo", "doing", "done"].map((status) => (
+          <div key={status}>
+            <h1>{status}</h1>
+            {todos
+              .filter((t) => t.i_status === status)
+              .map((todo) => (
+                <li key={todo.id}>
+                  <span>{todo.name} </span>
+                  <button onClick={() => changeStatus(todo.id, "todo")}>
+                    todo
+                  </button>
+                  <button onClick={() => changeStatus(todo.id, "doing")}>
+                    doing
+                  </button>
+                  <button onClick={() => changeStatus(todo.id, "done")}>
+                    done
+                  </button>
+                  <button onClick={() => deleteTodoItem(todo.id)}>
+                    Delete
+                  </button>
+                </li>
+              ))}
+          </div>
+        ))}
+      </ul>
     </div>
   );
 }
